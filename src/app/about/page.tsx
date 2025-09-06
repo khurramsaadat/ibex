eaa1c24d6f5e61b29dc67bd89795e031c70790ed
khurram <khurram.saadat@yahoo.com>
2025-09-06 12:57:47 +0400

    Fix Star icon reference error
    - Replaced remaining Star icon with ArrowRight icon in CTA section
    - Fixed Runtime ReferenceError: Star is not defined
    - Both CTA buttons now use ArrowRight icons consistently
    - About page should now load without any icon reference errors

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -312,7 +312,7 @@ export default function About() {
                 <ArrowRight className="ml-2 h-5 w-5" />
               </Button>
               <Button variant="outline" size="lg" className="text-lg px-8">
-                <Star className="mr-2 h-5 w-5" />
+                <ArrowRight className="mr-2 h-5 w-5" />
                 View Our Work
               </Button>
             </div>
